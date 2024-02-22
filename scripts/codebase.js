2063f6858749a4bfe9abcc4908dbb3a207fd0e51
JaniBT <janoscsordas072@gmail.com>
2024-02-22 12:37:15 +0100

    development started in codebase.html

---
+++ scripts/codebase.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".auto-input", {
+    strings: ["CodeBase", "Works for you", "is what you can count on"],
+    typeSpeed: 100,
+    backDelay: 500,
+    backSpeed: 100,
+    loop: true
+})
